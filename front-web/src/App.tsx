edee67fc04c4f484caff283cf13daf8411703ca8
Joao Pedro Haeser Iserhard <jiserhard@gmail.com>
2022-02-07 17:09:04 -0300

    feat: Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Navbar/index';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Navbar />
     </div>
   );
 }
